78d937a3a47e7dcf9103a4f4a0acbbc54a5952cd
montecamo <merrychristek@gmail.com>
2020-04-25 19:35:38 +0300

    fix build

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 cp -r nginx.build nginx
 
-find nginx -name "*.conf" -exec sed -i '' "s/{{DOMAIN}}/$1/g" {} \;
+find nginx -name "*.conf" -exec sed -i "s/{{DOMAIN}}/$1/g" {} \;
 
-sed -i '' "s/{{DOMAIN}}/$1/g" init-letsencrypt.sh
+sed -i "s/{{DOMAIN}}/$1/g" init-letsencrypt.sh
