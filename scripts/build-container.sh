8f80289b847a806cc80c07e99dd3585f9b635224
montecamo <merrychristek@gmail.com>
2021-10-17 22:01:54 +0300

    change deploy process

--- scripts/build-container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/home-page
-
-docker-compose build --no-cache $1
--- scripts/clone-projects.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/home-page
-
-git submodule init
-git submodule update --remote --recursive $1
